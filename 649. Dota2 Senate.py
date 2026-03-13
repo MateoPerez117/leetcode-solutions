d4c42cb8587a769ad2333a6eefc9542bbaba78c7
Mateo Perez <mateoperezr99@gmail.com>
2026-03-13 11:44:16 -0600

    add: Dota2 problem solution

--- 649. Dota2 Senate.py
+++ 649. Dota2 Senate.py
@@ -1,10 +1,28 @@
-# Input: senate = "RD"
-# Output: "Radiant"
+from collections import deque
 
 senate = "RDD"
-Rad = 0
-Dire = 0
+radiant = deque()
+dire = deque()
+n = len(senate)
 
 for i in range(len(senate)):
     if senate[i] == "R":
-        
+        radiant.append(i)
+    elif senate[i] == "D":
+        dire.append(i)
+
+while len(radiant) > 0 and len(dire) > 0:
+    if radiant[0] < dire[0]:
+        dire.popleft()
+        radiant.append(radiant[0] + n)
+        radiant.popleft()
+
+    else:
+        radiant.popleft()
+        dire.append(dire[0] + n)
+        dire.popleft()
+
+if len(radiant) > 0:
+    print ("Radiant")
+else:
+    print("Dire")
