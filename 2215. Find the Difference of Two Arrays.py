24b3061ee93e5c1010d6f43fddca74ad071b97d3
Mateo Perez <mateoperezr99@gmail.com>
2026-04-08 13:13:08 -0600

    feat new solution: 2215. Find the Difference of Two Arrays

--- 2215. Find the Difference of Two Arrays.py
+++ 2215. Find the Difference of Two Arrays.py
@@ -1,3 +1,4 @@
+#first solution
 nums1 = [1,2,3]
 nums2 = [2,4,6]
 ans1 = []
@@ -13,3 +14,17 @@ for i in range(len(nums2)):
     
 print (ans1, ans2)
 
+# better solution   
+
+nums1Set, nums2Set = set(nums1), set(nums2)
+ans1, ans2 = set(), set()
+
+for n in nums1:
+    if n not in nums2Set:
+        ans1.add(n)
+
+for n in nums2:
+    if n not in nums1Set:
+        ans2.add(n)
+
+print [list(ans1), list(ans2)]
